2f33f32b995782d23f9f99b27324d97f8996930e
vpro3611 <vprov8645@gmail.com>
2026-03-02 22:32:23 +0100

    Entry point: for now just testing database con

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
+import {pool} from "./database";
+
+
+
 console.log('Happy developing ✨')
+
+
+async function main() {
+    console.log(process.env.DATABASE_URL)
+    const res = await pool.query('SELECT NOW()');
+    console.log(res.rows[0]);
+}
+
+main().catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
