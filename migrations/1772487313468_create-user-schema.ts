0740561a43883f332bac997427ba1aaa42bd6c5d
vpro3611 <vprov8645@gmail.com>
2026-03-02 23:01:37 +0100

    Add: added a new migration - creating user table with initial schema

---
+++ migrations/1772487313468_create-user-schema.ts
@@ -0,0 +1,50 @@
+import type { ColumnDefinitions, MigrationBuilder } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+    pgm.createTable('users', {
+        id: {
+            type: "uuid",
+            primaryKey: true,
+        },
+        username: {
+            type: "varchar(255)",
+            notNull: true,
+            unique: true,
+        },
+        email: {
+            type: "varchar(255)",
+            notNull: true,
+            unique: true,
+        },
+        password_hash: {
+            type: "varchar(255)",
+            notNull: true,
+        },
+        is_active: {
+            type: "boolean",
+            notNull: true,
+            default: true,
+        },
+        avatar_url: {
+            type: "text",
+            notNull: false,
+        },
+        last_seen_at: {
+            type: "timestamp",
+            notNull: false,
+        },
+        created_at: {
+            type: "timestamp",
+            notNull: true,
+        },
+        updated_at: {
+            type: "timestamp",
+            notNull: true,
+            default: pgm.func('now()'),
+        },
+    })
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {}
