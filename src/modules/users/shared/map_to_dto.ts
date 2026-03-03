34a3006810d2bbe5a219a0e4b6968753d05fdd7f
vpro3611 <vprov8645@gmail.com>
2026-03-03 23:38:39 +0100

    Refactor : made separate classes for share methods, so now they do not overlap, make testing easier

--- src/modules/users/shared/map_to_dto.ts
+++ src/modules/users/shared/map_to_dto.ts
@@ -1,15 +1,18 @@
 import {User} from "../domain/user";
-import {UserDTO} from "../infrastructure/user_dto";
+import {UserDTO} from "../DTO/user_dto";
 
-export function SHAREDmapToDto(user: User): UserDTO
-{
-    return {
-        id: user.id,
-        username: user.getUsername().getValue(),
-        email: user.getEmail().getValue(),
-        isActive: user.getIsActive(),
-        lastSeenAt: user.getLastSeenAt().toISOString(),
-        createdAt: user.getCreatedAt().toISOString(),
-        updated_at: user.getUpdatedAt().toISOString(),
+
+export class UserMapper {
+    async mapToDto(user: User): Promise<UserDTO> {
+        return {
+            id: user.id,
+            username: user.getUsername().getValue(),
+            email: user.getEmail().getValue(),
+            isActive: user.getIsActive(),
+            isVerified: user.getIsVerified(),
+            lastSeenAt: user.getLastSeenAt().toISOString(),
+            createdAt: user.getCreatedAt().toISOString(),
+            updated_at: user.getUpdatedAt().toISOString(),
+        }
     }
 }
--- src/modules/users/shared/user_exists_by_id.ts
+++ src/modules/users/shared/user_exists_by_id.ts
@@ -2,11 +2,14 @@ import {UserRepoReader} from "../ports/user_repo_interfaces";
 import {UserNotFoundError} from "../errors/use_case_errors";
 
 
-export async function SHAREDUserExistsById(id: string, userRepo: UserRepoReader)
-{
-    const user = await userRepo.getUserById(id);
-    if (!user) {
-        throw new UserNotFoundError(`User not found`);
+export class UserLookup {
+    constructor(private readonly userRepoReader: UserRepoReader) {}
+
+    async getUserOrThrow(id: string) {
+        const user = await this.userRepoReader.getUserById(id);
+        if (!user) {
+            throw new UserNotFoundError(`User not found`);
+        }
+        return user;
     }
-    return user;
 }
