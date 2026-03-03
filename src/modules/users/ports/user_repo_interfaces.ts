6ba3eabfa6ee1c027c527097b6c3faeeae08b0d6
vpro3611 <vprov8645@gmail.com>
2026-03-03 02:11:03 +0100

    Add: added new method to user repo reader - find user by email

--- src/modules/users/ports/user_repo_interfaces.ts
+++ src/modules/users/ports/user_repo_interfaces.ts
@@ -3,6 +3,7 @@ import { User } from "../domain/user";
 export interface UserRepoReader {
     getUserById(id: string): Promise<User | null>;
     getUserByUsername(username: string): Promise<User | null>;
+    getUserByEmail(email: string): Promise<User | null>;
 }
 
 export interface UserRepoWriter {
