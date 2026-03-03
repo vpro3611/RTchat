6e4dea4ca7e8d3c067ed0a7a2dadc7cc99c4b06a
vpro3611 <vprov8645@gmail.com>
2026-03-03 02:15:24 +0100

    Add: added infra folder, where all additional infra stuff will be (for now only bcrypter)

---
+++ src/modules/users/infrastructure/user_dto.ts
@@ -0,0 +1,11 @@
+
+
+export type UserDTO = {
+    id: string;
+    username: string;
+    email: string;
+    isActive: boolean;
+    lastSeenAt: string,
+    createdAt: string,
+    updated_at: string,
+}
