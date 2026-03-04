0f4b8b24d2792ed3391a883d2db60d3be39b216a
vpro3611 <vprov8645@gmail.com>
2026-03-04 18:36:04 +0100

    Refactor: now it differentiates and also checks whether there is an error (email already exists in db) from db, and maps it to domain. Other errors are passed outside

--- src/modules/users/application/change_email_use_case.ts
+++ src/modules/users/application/change_email_use_case.ts
@@ -3,6 +3,7 @@ import {Email} from "../domain/email";
 import {EmailAlreadyExistsError} from "../errors/email_errors";
 import {UserMapper} from "../shared/map_to_dto";
 import {UserLookup} from "../shared/user_exists_by_id";
+import {EmailAlreadyExistDatabaseError, UserDatabaseError} from "../errors/user_database_error";
 
 
 export class ChangeEmailUseCase {
@@ -22,16 +23,23 @@ export class ChangeEmailUseCase {
     }
 
     async changeEmailUseCase(actorId: string, newEmail: string) {
-        const newEmailValid = Email.create(newEmail);
+        try {
+            const newEmailValid = Email.create(newEmail);
 
-        const user = await this.userLookup.getUserOrThrow(actorId);
+            const user = await this.userLookup.getUserOrThrow(actorId);
 
-        await this.checkUserWithSameEmail(newEmailValid.getValue());
+            await this.checkUserWithSameEmail(newEmailValid.getValue());
 
-        user.setEmail(newEmailValid);
+            user.setEmail(newEmailValid);
 
-        const saved = await this.userRepoWriter.save(user);
+            const saved = await this.userRepoWriter.save(user);
 
-        return this.mapper.mapToDto(saved);
+            return this.mapper.mapToDto(saved);
+        } catch (error) {
+            if (error instanceof EmailAlreadyExistDatabaseError) {
+                throw new EmailAlreadyExistsError(`User with this email already exists`);
+            }
+            throw error;
+        }
     }
 }
