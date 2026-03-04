fe6f1b529787f389a2f82303e42b0ce3aef71d72
vpro3611 <vprov8645@gmail.com>
2026-03-04 01:22:53 +0100

    Fix: fixed error and in case of dropping in also drops consrtraints

--- migrations/1772580098390_create-refresh-tokens-repo.ts
+++ migrations/1772580098390_create-refresh-tokens-repo.ts
@@ -7,9 +7,9 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
         id UUID PRIMARY KEY,
         user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
         token_hash TEXT NOT NULL,
-        expires_at TIMESTAMP NOT NULL,
-        revoked_at TIMESTAMP NULL,
-        created_at TIMESTAMP NOT NULL DEFAULT NOW()
+        expires_at TIMESTAMPTZ NOT NULL,
+        revoked_at TIMESTAMPTZ NULL,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
     )`);
     pgm.sql(`CREATE INDEX idx_refresh_user_id ON refresh_tokens (user_id)`);
     pgm.sql(`CREATE INDEX idx_refresh_token_hash ON refresh_tokens (token_hash)`)
@@ -17,4 +17,6 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
     pgm.dropTable('refresh_tokens');
+    pgm.dropIndex('refresh_tokens', 'idx_refresh_user_id');
+    pgm.dropIndex('refresh_tokens', 'idx_refresh_token_hash');
 }
