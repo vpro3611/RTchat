5576d144fb422130f75e99a5d6b793fba00fcf75
vpro3611 <vprov8645@gmail.com>
2026-03-04 19:24:05 +0100

    Add: added logout controller with cookies

---
+++ src/modules/authentification/controllers/logout_controller.ts
@@ -0,0 +1,25 @@
+import {AuthService} from "../auth_service";
+import {Request, Response} from "express";
+
+export class LogoutController {
+    constructor(private readonly authService: AuthService) {}
+
+
+    logoutController = async (req: Request, res: Response) => {
+        const refreshToken = req.cookies.refreshToken;
+
+        if (!refreshToken) {
+            throw new Error("Refresh token not found");
+        }
+
+        await this.authService.logout(refreshToken);
+
+        res.clearCookie("refreshToken", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV! === "production",
+            sameSite: process.env.NODE_ENV! === "production" ? "strict" : "lax",
+        });
+
+        res.status(204).send();
+    }
+}
