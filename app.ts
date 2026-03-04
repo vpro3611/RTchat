ed8d71cfe219e591e042d9dd7a2409c5338d6c78
vpro3611 <vprov8645@gmail.com>
2026-03-04 21:05:19 +0100

    Add: added basic app, here goest all routes and middlewares, will be finished later

---
+++ app.ts
@@ -0,0 +1,23 @@
+import {AppContainer} from "./container";
+import express, {Express} from "express";
+import cookieParser from "cookie-parser";
+
+
+export function createApp(dependencies: AppContainer): Express
+{
+    const app = express();
+
+    // loger middleware here.
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
+
+    const publicRouter = express.Router();
+    const privateRouter = express.Router();
+
+    app.use("/public", publicRouter);
+    app.use("/private", privateRouter);
+
+    return app;
+}
