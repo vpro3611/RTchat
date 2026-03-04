35870be5af9e645b68dfdd1cf550a0c10926f7eb
vpro3611 <vprov8645@gmail.com>
2026-03-04 19:25:00 +0100

    Add: added refresh controller, with cookies

---
+++ src/modules/authentification/controllers/refresh_controller.ts
@@ -0,0 +1,23 @@
+import {AuthService} from "../auth_service";
+import {Request, Response} from "express";
+
+export class RefreshController {
+    constructor(private readonly authService: AuthService) {}
+
+    refreshController = async (req: Request, res: Response) => {
+        const refreshToken = req.cookies.refreshToken;
+
+        if (!refreshToken) {
+            throw new Error("Refresh token not found");
+        }
+
+        const tokens = await this.authService.refresh(refreshToken);
+
+        res.cookie("refreshToken", tokens.refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV! === "production",
+            sameSite: process.env.NODE_ENV! === "production" ? "strict" : "lax",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        }).json({accessToken: tokens.accessToken})
+    }
+}
