983b4a7ed158b5dd0771bfa07b208a1a5e9118df
vpro3611 <vprov8645@gmail.com>
2026-03-06 23:00:57 +0100

    Add: added participant repo realization for participants of chat

---
+++ src/modules/chat/repositories_pg_realization/participant_repository_pg.ts
@@ -0,0 +1,111 @@
+import {ParticipantRepoInterface} from "../domain/ports/participant_repo_interface";
+import {Pool, PoolClient} from "pg";
+import {Participant} from "../domain/participant/participant";
+
+
+export class ParticipantRepositoryPg implements ParticipantRepoInterface {
+    constructor(private readonly pool: Pool | PoolClient) {}
+
+    private mapToParticipant(row: any): Participant {
+        return Participant.restore(
+            row.conversation_id,
+            row.user_id,
+            row.role,
+            row.can_send_messages,
+            row.muted_until,
+            row.joined_at,
+        );
+    }
+
+    async save(participant: Participant): Promise<void> {
+        await this.pool.query(
+            `
+            INSERT INTO conversation_participants
+            (conversation_id, user_id, role, can_send_messages, muted_until, joined_at)
+            VALUES ($1, $2, $3, $4, $5, $6)
+            
+            ON CONFLICT (conversation_id, user_id) 
+            DO UPDATE SET 
+                role = EXCLUDED.role,
+                can_send_messages = EXCLUDED.can_send_messages,
+                muted_until = EXCLUDED.muted_until,
+            `,
+            [
+                participant.getConversationId(),
+                participant.userId,
+                participant.getRole(),
+                participant.getCanSendMessages(),
+                participant.getMutedUntil(),
+                participant.getJoinedAt(),
+            ]
+        )
+    }
+
+    async remove(conversationId: string, userId: string): Promise<void> {
+        await this.pool.query(`DELETE FROM conversation_participants WHERE conversation_id = $1 AND user_id = $2`,
+            [conversationId, userId]);
+    }
+
+    async findParticipant(conversationId: string, userId: string): Promise<Participant | null> {
+        const result = await this.pool.query(
+            `
+            SELECT * FROM conversation_participants WHERE conversation_id = $1 AND user_id = $2
+            `,
+            [conversationId, userId]
+        )
+
+        if (!result.rows.length) {
+            return null;
+        }
+
+        const row = result.rows[0];
+
+        return this.mapToParticipant(row);
+    }
+
+    async exists(conversationId: string, userId: string): Promise<boolean> {
+        const result = await this.pool.query(
+            `
+            SELECT 1 FROM conversation_participants WHERE conversation_id = $1 AND user_id = $2
+            `,
+            [conversationId, userId]
+        )
+        return result.rows.length > 0;
+    }
+
+    async getParticipants(conversationId: string, limit = 20, cursor?: string): Promise<{items: Participant[], nextCursor?: string}> {
+        const params: any[] = [conversationId];
+        let cursorCondition = "";
+
+        if (cursor) {
+            params.push(cursor);
+            cursorCondition = `AND joined_at > $${params.length}`;
+        }
+
+        params.push(limit+1);
+
+        const result = await this.pool.query(
+            `
+            SELECT * FROM conversation_participants 
+            WHERE conversation_id = $1
+            ${cursorCondition}
+            ORDER BY joined_at DESC
+            LIMIT $${params.length}
+            `,
+            params
+        )
+
+        const rows = result.rows;
+
+        let nextCursor: string | undefined;
+
+        if (rows.length > limit) {
+            const next = rows.pop();
+            nextCursor = next?.joined_at.toISOString();
+        }
+
+        const items = rows.map(row => this.mapToParticipant(row));
+
+        return {items, nextCursor};
+    }
+}
