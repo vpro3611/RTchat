6ec584b7937b57aebdc66db32d0d9cf89614df27
vpro3611 <vprov8645@gmail.com>
2026-03-08 21:14:54 +0100

    Add: added custom errors types for message layer

---
+++ src/modules/chat/application/errors/message_errors/message_errors.ts
@@ -0,0 +1,26 @@
+import {AuthorizationError, ConflictError, NotFoundError, ValidationError} from "../../../../../http_errors_base";
+
+export class MessageNotFoundError extends NotFoundError {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class MessageNotAPartOfConversationError extends ValidationError {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+
+export class InvalidMessageError extends ValidationError {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class CannotEditMessageError extends ConflictError {
+    constructor(message: string) {
+        super(message);
+    }
+}
