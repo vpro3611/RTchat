4061438f7302f3ad93e18388d94da4f0134931e6
vpro3611 <vprov8645@gmail.com>
2026-03-08 19:50:25 +0100

    Add: added custom error

--- src/modules/chat/shared/is_participant.ts
+++ src/modules/chat/shared/is_participant.ts
@@ -1,4 +1,5 @@
 import {ParticipantRepoInterface} from "../domain/ports/participant_repo_interface";
+import {ParticipantNotFoundError} from "../application/errors/participants_errors/participant_errors";
 
 
 export class CheckIsParticipant {
@@ -7,7 +8,7 @@ export class CheckIsParticipant {
     async checkIsParticipant(actorId: string, conversationId: string) {
         const participant = await this.participantRepo.findParticipant(conversationId, actorId);
         if (!participant) {
-            throw new Error("User is not a participant of the conversation");
+            throw new ParticipantNotFoundError("User is not a participant of the conversation");
         }
         return participant;
     }
