3b113e27d2ae2a22cddb75bfb65b3c6ed052a3b4
vpro3611 <vprov8645@gmail.com>
2026-03-08 19:38:08 +0100

    Add: added error handling via pg error mapper

--- src/modules/chat/repositories_pg_realization/message_repository_pg.ts
+++ src/modules/chat/repositories_pg_realization/message_repository_pg.ts
@@ -1,6 +1,7 @@
 import {MessageRepoInterface} from "../domain/ports/message_repo_interface";
 import {Pool, PoolClient} from "pg";
 import {Message} from "../domain/message/message";
+import {mapPgError} from "../../error_mapper/pg_error_mapper";
 
 
 export class MessageRepositoryPg implements MessageRepoInterface {
@@ -20,84 +21,107 @@ export class MessageRepositoryPg implements MessageRepoInterface {
     }
 
     async create(message: Message): Promise<void> {
-        await this.pg.query(
-            `
-            INSERT INTO messages
-            (id, conversation_id, sender_id, content, is_edited, is_deleted, created_at, updated_at)
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-            `,
-            [
-                message.id,
-                message.getConversationId(),
-                message.getSenderId(),
-                message.getContent(),
-                message.getIsEdited(),
-                message.getIsDeleted(),
-                message.getCreatedAt(),
-                message.getUpdatedAt(),
-            ]
-        );
+        try {
+            await this.pg.query(
+                `
+                    INSERT INTO messages
+                    (id, conversation_id, sender_id, content, is_edited, is_deleted, created_at, updated_at)
+                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+                `,
+                [
+                    message.id,
+                    message.getConversationId(),
+                    message.getSenderId(),
+                    message.getContent(),
+                    message.getIsEdited(),
+                    message.getIsDeleted(),
+                    message.getCreatedAt(),
+                    message.getUpdatedAt(),
+                ]
+            );
+        } catch (error) {
+            throw mapPgError(error);
+        }
     }
 
     async findById(id: string): Promise<Message | null> {
-        const result = await this.pg.query(`SELECT * FROM messages WHERE id = $1`,
-            [id]);
+        try {
+            const result = await this.pg.query(`SELECT *
+                                                FROM messages
+                                                WHERE id = $1`,
+                [id]);
 
-        if (!result.rows.length) {
-            return null;
+            if (!result.rows.length) {
+                return null;
+            }
+
+            const row = result.rows[0];
+
+            return this.mapToMessage(row);
+        } catch (error) {
+            throw mapPgError(error);
         }
-
-        const row = result.rows[0];
-
-        return this.mapToMessage(row);
     }
 
     async findByConversationId(conversationId: string, limit = 20, cursor?: string): Promise<{items: Message[], nextCursor?: string}> {
-        const params: any[] = [conversationId];
-        let cursorCondition = "";
+        try {
+            const params: any[] = [conversationId];
+            let cursorCondition = "";
 
-        if (cursor) {
-            params.push(cursor);
-            cursorCondition = `AND created_at > $${params.length}`;
+            if (cursor) {
+                params.push(cursor);
+                cursorCondition = `AND created_at > $${params.length}`;
+            }
+
+            params.push(limit + 1);
+
+            const result = await this.pg.query(
+                `
+                    SELECT *
+                    FROM messages
+                    WHERE conversation_id = $1
+                        ${cursorCondition}
+                    ORDER BY created_at DESC
+                        LIMIT $${params.length}
+                `,
+                params
+            );
+
+            const rows = result.rows;
+
+            let nextCursor: string | undefined;
+
+            if (rows.length > limit) {
+                const nextItem = rows.pop();
+                nextCursor = nextItem?.created_at.toISOString();
+            }
+
+            const items = rows.map(row => this.mapToMessage(row));
+
+            return {items, nextCursor};
+        } catch (error) {
+            throw mapPgError(error);
         }
-
-        params.push(limit+1);
-
-        const result = await this.pg.query(
-            `
-            SELECT * 
-            FROM messages
-            WHERE conversation_id = $1
-            ${cursorCondition}
-            ORDER BY created_at DESC
-            LIMIT $${params.length}
-            `,
-            params
-        );
-
-        const rows = result.rows;
-
-        let nextCursor: string | undefined;
-
-        if (rows.length > limit) {
-            const nextItem = rows.pop();
-            nextCursor = nextItem?.created_at.toISOString();
-        }
-
-        const items = rows.map(row => this.mapToMessage(row));
-
-        return {items, nextCursor};
     }
 
     async update(message: Message): Promise<void> {
-        await this.pg.query(`UPDATE messages SET content = $1, is_edited = $2, is_deleted = $3, updated_at = $4 WHERE id = $5`,
-            [
-                message.getContent(),
-                message.getIsEdited(),
-                message.getIsDeleted(),
-                message.getUpdatedAt(),
-                message.id,
-            ]
-        );
+        try {
+            await this.pg.query(`UPDATE messages
+                                 SET content = $1,
+                                     is_edited = $2,
+                                     is_deleted = $3,
+                                     updated_at = $4
+                                 WHERE id = $5`,
+                [
+                    message.getContent(),
+                    message.getIsEdited(),
+                    message.getIsDeleted(),
+                    message.getUpdatedAt(),
+                    message.id,
+                ]
+            );
+        } catch (error) {
+            throw mapPgError(error);
+        }
     }
 }
