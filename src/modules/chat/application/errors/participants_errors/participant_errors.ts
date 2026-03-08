eac3dda9a9019b3791fa1dddd00d86d075d3eb86
vpro3611 <vprov8645@gmail.com>
2026-03-08 21:14:28 +0100

    Add: added custom errors types for participation layers

---
+++ src/modules/chat/application/errors/participants_errors/participant_errors.ts
@@ -0,0 +1,51 @@
+import {AuthorizationError, ConflictError, NotFoundError} from "../../../../../http_errors_base";
+
+
+export class ParticipantNotFoundError extends NotFoundError {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class UserIsNotAllowedToPerformError extends AuthorizationError {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class UserIsNotAnAuthorError extends AuthorizationError {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class ActorIsNotParticipantError extends AuthorizationError {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class UserIsNotParticipantError extends AuthorizationError {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class UserAlreadyParticipantError extends ConflictError {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class CannotJoinDirectConversationError extends ConflictError {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class InsufficientPermissionsError extends AuthorizationError {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
