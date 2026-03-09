3f0c40fe08061f78854657f1554eadf9ee945579
vpro3611 <vprov8645@gmail.com>
2026-03-09 17:09:01 +0100

    Add: added tx layer for message, ensuring all run in a single transaction, withing a single connection

---
+++ src/modules/chat/transactional_services/message/edit_message_service.ts
@@ -0,0 +1,28 @@
+import {
+    TransactionManagerInterface
+} from "../../../infrasctructure/ports/transaction_manager/transaction_manager_interface";
+import {MessageRepositoryPg} from "../../repositories_pg_realization/message_repository_pg";
+import {MapToMessage} from "../../shared/map_to_message";
+import {ParticipantRepositoryPg} from "../../repositories_pg_realization/participant_repository_pg";
+import {CheckIsParticipant} from "../../shared/is_participant";
+import {FindMessageById} from "../../shared/find_message_by_id";
+import {EditMessageUseCase} from "../../application/message/edit_message_use_case";
+
+
+export class EditMessageTxService {
+    constructor(private readonly txManager: TransactionManagerInterface) {}
+
+    async editMessageTxService(actorId: string, conversationId: string, messageId: string, newContent: string) {
+        return await this.txManager.runInTransaction(async (client) => {
+            const messageRepo = new MessageRepositoryPg(client);
+            const messageMapper = new MapToMessage();
+            const participantRepo = new ParticipantRepositoryPg(client);
+            const checkIsParticipant = new CheckIsParticipant(participantRepo);
+            const findMessageById = new FindMessageById(messageRepo);
+
+            const editMessageUseCase = new EditMessageUseCase(messageRepo, messageMapper, checkIsParticipant, findMessageById);
+
+            return await editMessageUseCase.editMessageUseCase(actorId, conversationId, messageId, newContent);
+        })
+    }
+}
---
+++ src/modules/chat/transactional_services/message/send_message_service.ts
@@ -0,0 +1,29 @@
+import {
+    TransactionManagerInterface
+} from "../../../infrasctructure/ports/transaction_manager/transaction_manager_interface";
+import {MessageRepositoryPg} from "../../repositories_pg_realization/message_repository_pg";
+import {ConversationRepositoryPg} from "../../repositories_pg_realization/conversation_repository_pg";
+import {MapToMessage} from "../../shared/map_to_message";
+import {ParticipantRepositoryPg} from "../../repositories_pg_realization/participant_repository_pg";
+import {CheckIsParticipant} from "../../shared/is_participant";
+import {SendMessageUseCase} from "../../application/message/send_message_use_case";
+
+
+export class SendMessageTxService {
+    constructor(private readonly txManager: TransactionManagerInterface) {}
+
+
+    async sendMessageTxService(actorId: string, conversationId: string, content: string) {
+        return await this.txManager.runInTransaction(async (client) => {
+            const messageRepo = new MessageRepositoryPg(client);
+            const conversationRepo = new ConversationRepositoryPg(client);
+            const messageMapper = new MapToMessage();
+            const participantRepo = new ParticipantRepositoryPg(client);
+            const checkIsParticipant = new CheckIsParticipant(participantRepo);
+
+            const sendMessageUseCase = new SendMessageUseCase(messageRepo, conversationRepo, messageMapper, checkIsParticipant);
+
+            return await sendMessageUseCase.sendMessageUseCase(actorId, conversationId, content);
+        })
+    }
+}
