6a325beb7ef3210cc20624145e0e609967b62833
vpro3611 <vprov8645@gmail.com>
2026-03-10 20:17:08 +0100

    Add: added chat gateway to startServer(), now both http and web socket start together

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,28 @@
 import {assembleContainer} from "./container";
 import {createApp} from "./app";
+import {ChatGateway} from "./modules/chat/web_socket/chat_gateway";
+import * as http from "node:http";
 
 
 export async function startServer() {
     const dependencies = assembleContainer();
     const app = createApp(dependencies);
 
+    const server = http.createServer(app);
+
     const port = process.env.PORT || 3000;
 
-    app.listen(port, () => {
+    new ChatGateway(
+        server,
+        dependencies.jwtTokenService,
+        dependencies.sendMessageService,
+        dependencies.editMessageService,
+        dependencies.deleteMessageService,
+        dependencies.getUserConversationsService,
+        dependencies.markConversationReadService,
+    )
+
+    server.listen(port, () => {
         console.log(`Server is running on port ${port}! Happy developing!`);
     });
 }
