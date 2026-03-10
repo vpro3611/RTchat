4ce57c02f5a48ae46494d7aa7d682ac340e2cd7f
vpro3611 <vprov8645@gmail.com>
2026-03-10 19:37:25 +0100

    Add: added types for chat gateway, making it easier to handle web sockets

---
+++ src/modules/chat/web_socket/chat_gateway_types.ts
@@ -0,0 +1,59 @@
+import {MessageDTO} from "../DTO/message_dto";
+import {AccessTokenPayload} from "../../authentification/payloads/payloads";
+
+export interface ClientToServerEvents {
+
+    "conversation:join": (data: {
+        conversationId: string
+    }) => void
+
+    "message:send": (data: {
+        conversationId: string
+        content: string
+    }) => void
+
+    "message:edit": (data: {
+        conversationId: string,
+        messageId: string,
+        newContent: string
+    }) => void
+
+    "message:delete": (data: {
+        conversationId: string,
+        messageId: string,
+    }) => void
+
+    "typing:start": (data: {
+        conversationId: string
+    }) => void
+
+    "typing:stop": (data: {
+        conversationId: string
+    }) => void
+
+    "message:read": (data: {
+        conversationId: string,
+        messageId: string
+    }) => void
+}
+
+export interface ServerToClientEvents {
+    "message:new": (message: MessageDTO) => void
+    "message:edited": (message: MessageDTO) => void
+    "message:deleted": (message: MessageDTO) => void
+
+    "user:online": (data: {userId: string}) => void
+    "user:offline": (data: {userId: string}) => void
+
+    "typing:start": (data: {userId: string, conversationId: string}) => void
+    "typing:stop": (data: {userId: string, conversationId: string}) => void
+
+    "message:read": (data: {userId: string, conversationId: string, messageId: string}) => void
+
+    "error": (error: {message: string}) => void
+
+}
+
+export interface SocketData {
+    userId: AccessTokenPayload,
+}
