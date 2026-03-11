6485174da3703585e117f985373f4e71a4e96578
vpro3611 <vprov8645@gmail.com>
2026-03-11 17:31:53 +0100

    Add: added controllers for handling typing for web socket

---
+++ src/modules/chat/web_socket_controllers/start_typing_controller.ts
@@ -0,0 +1,21 @@
+import {AuthSocket} from "../web_socket/chat_gateway_types";
+import {z} from "zod";
+import {Server} from "socket.io";
+
+export const StartTypingSchema = z.object({
+    conversationId: z.string().uuid(),
+})
+
+export class StartTypingController {
+
+    constructor(private io: Server) {}
+
+    startTypingController = async (socket: AuthSocket, conversationId: string)=>  {
+        const userId = socket.data.userId;
+
+        this.io.to(conversationId).emit("typing:started", {
+            userId: userId.sub,
+            conversationId: conversationId,
+        });
+    }
+}
---
+++ src/modules/chat/web_socket_controllers/stop_typing_controller.ts
@@ -0,0 +1,22 @@
+import {Server} from "socket.io";
+import {AuthSocket} from "../web_socket/chat_gateway_types";
+import {z} from "zod";
+
+export const StopTypingSchema = z.object({
+    conversationId: z.string().uuid(),
+});
+
+
+export class StopTypingController {
+    constructor(private readonly io: Server) {}
+
+
+    stopTypingController = async (socket: AuthSocket, conversationId: string) => {
+        const userId = socket.data.userId;
+
+        this.io.to(conversationId).emit("typing:stopped", {
+            userId: userId.sub,
+            conversationId: conversationId,
+        });
+    }
+}
