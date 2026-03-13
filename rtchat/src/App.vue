1344297fdac9a47c56cae07ddc2c24df4d360995
vpro3611 <vprov8645@gmail.com>
2026-03-14 00:19:15 +0100

    Add: added router view to app and onMount so it checks for user

--- rtchat/src/App.vue
+++ rtchat/src/App.vue
@@ -1,7 +1,34 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import {onMounted} from "vue";
+import {AuthApi} from "./api/apis/auth_api.ts";
+import {AuthStore} from "./stores/auth_store.ts";
+
+onMounted(async () => {
+  try {
+    const refreshData = await AuthApi.refresh();
+
+    if (refreshData?.accessToken) {
+      AuthStore.setToken(refreshData?.accessToken, null);
+      const user = await AuthApi.me();
+      if (!user) {
+        AuthStore.clearToken();
+        return;
+      }
+      AuthStore.setUser(user);
+    }
+  } catch (e) {
+    AuthStore.clearToken();
+    console.error(e);
+  } finally {
+    AuthStore.finishBootstrapping();
+  }
+})
+
+
 </script>
 
+
+
 <template>
-  <HelloWorld />
+  <router-view />
 </template>
