edf7f104a34d4562af35a49ed74b6ca76c8f2186
vpro3611 <vprov8645@gmail.com>
2026-03-15 22:40:01 +0100

    Layouts (auth and main for now)

---
+++ frontend-chat/src/layouts/AuthLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-page-container>
+
+      <q-page class="flex flex-center bg-grey-2">
+
+        <div style="width:100%; max-width:900px;">
+          <router-view />
+        </div>
+
+      </q-page>
+
+    </q-page-container>
+
+  </q-layout>
+</template>
