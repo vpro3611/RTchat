7c002ae1a6dd352a5f143e45b26a4cf62d328e6f
vpro3611 <vprov8645@gmail.com>
2026-03-15 22:40:13 +0100

    Router

---
+++ frontend-chat/src/router/routes.ts
@@ -0,0 +1,45 @@
+import AuthPage from "../pages/AuthPage.vue";
+import MainPage from "../pages/MainPage.vue";
+import VerifyEmailNotice from "../pages/VerifyEmailNotice.vue";
+import EmailVerified from "../pages/EmailVerified.vue";
+import LoginPage from "pages/LoginPage.vue";
+
+
+
+const routes = [
+
+  {
+    path: '/',
+    redirect: '/auth'
+  },
+
+  {
+    path: '/',
+    component: () => import('layouts/AuthLayout.vue'),
+    children: [
+
+      { path: 'auth', component: AuthPage },
+
+      { path: 'login', component: LoginPage },
+
+      { path: 'verify-email', component: VerifyEmailNotice },
+
+      { path: 'email-verified', component: EmailVerified }
+
+    ]
+  },
+
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+
+      { path: 'main', component: MainPage }
+
+    ]
+  }
+
+]
+
+
+export default routes;
