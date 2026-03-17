64eacdb7db368456d4b1d7964786ced6e328d7bd
vpro3611 <vprov8645@gmail.com>
2026-03-17 03:01:24 +0100

    Update: fully updated chat page

--- frontend-chat/src/pages/ChatPage.vue
+++ frontend-chat/src/pages/ChatPage.vue
@@ -1,5 +1,46 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router"
+import {ref} from "vue";
+const message = ref("")
+const route = useRoute()
+
+function send() {
+  if (!message.value.trim()) return
+
+  console.log("Sending message:", message.value)
+
+  message.value = ""
+}
+
+</script>
+
 <template>
-  <q-page>
-    Chat ID: {{ $route.params.id }}
-  </q-page>
+  <div class="column full-height">
+
+    <!-- HEADER -->
+    <div class="q-pa-md border-bottom">
+      <div class="text-h6">
+        Chat {{ route.params.id }}
+      </div>
+    </div>
+
+    <!-- MESSAGES -->
+    <div class="col q-pa-md">
+      <div class="text-grey">
+        Messages will be here
+      </div>
+    </div>
+
+    <!-- INPUT -->
+    <div class="q-pa-md">
+      <q-input
+        v-model="message"
+        @keyup.enter="send"
+        dense
+        outlined
+        placeholder="Type a message..."
+      />
+    </div>
+
+  </div>
 </template>
