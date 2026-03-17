db1ae67fac9ad34813bd5e9c3b9aefcc942cda7d
vpro3611 <vprov8645@gmail.com>
2026-03-17 01:24:30 +0100

    Add: added new route for redirecting to conversation page'

--- frontend-chat/src/router/routes.ts
+++ frontend-chat/src/router/routes.ts
@@ -35,6 +35,7 @@ const routes = [
 
       { path: 'profile', component: () => import('pages/ProfilePage.vue') },
 
+      { path: 'chat/:id', component: () => import('pages/ChatPage.vue')},
     ]
   }
 
