44091cf75c9db9a9d910c9d6aedd73c21903e06e
vpro3611 <vprov8645@gmail.com>
2026-03-18 02:30:21 +0100

    Add: added new method for new feature for users - searching them by username

--- src/modules/users/ports/user_repo_interfaces.ts
+++ src/modules/users/ports/user_repo_interfaces.ts
@@ -4,6 +4,7 @@ export interface UserRepoReader {
     getUserById(id: string): Promise<User | null>;
     getUserByUsername(username: string): Promise<User | null>;
     getUserByEmail(email: string): Promise<User | null>;
+    searchUsers(query: string, limit?: number, cursor?: string): Promise<{items: User[], nextCursor?: string}>;
 }
 
 export interface UserRepoWriter {
