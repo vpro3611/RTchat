1b6a2bc31d0a48ae2f774dbc965f8e0c079905b5
vpro3611 <vprov8645@gmail.com>
2026-03-20 01:06:32 +0100

    Update: updated tx layer accroding to changes

--- src/modules/users/transactional_services/change_email_tx_service.ts
+++ src/modules/users/transactional_services/change_email_tx_service.ts
@@ -6,6 +6,11 @@ import {UserRepoWriterPg} from "../repositories/user_repo_writer_pg";
 import {UserMapper} from "../shared/map_to_dto";
 import {UserLookup} from "../shared/user_exists_by_id";
 import {ChangeEmailUseCase} from "../application/change_email_use_case";
+import {SendVerifEmailShared} from "../shared/send_verif_email_shared";
+import {EmailSenderNodemailer} from "../../infrasctructure/ports/email_verif_infra/email_sender/email_sender";
+import {
+    EmailVerificationTokenRepoPg
+} from "../../infrasctructure/ports/email_verif_infra/email_verification_token_repo/email_verification_token_repo_pg";
 
 
 export class ChangeEmailTxService {
@@ -19,8 +24,18 @@ export class ChangeEmailTxService {
             const mapper = new UserMapper();
             const userLookup = new UserLookup(userRepoReader);
 
+            const emailSender = new EmailSenderNodemailer();
+            const emailVerifRepo = new EmailVerificationTokenRepoPg(client);
 
-            const changeEmailUseCase = new ChangeEmailUseCase(userRepoReader, userRepoWriter, mapper, userLookup);
+            const sendVerifShared = new SendVerifEmailShared(emailSender, emailVerifRepo);
+
+            const changeEmailUseCase = new ChangeEmailUseCase(
+                userRepoReader,
+                userRepoWriter,
+                mapper,
+                userLookup,
+                sendVerifShared
+            );
 
             return await changeEmailUseCase.changeEmailUseCase(actorId, newEmail);
         })
