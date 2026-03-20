473742061431d60c901c0e8e47259b2c0f545636
vpro3611 <vprov8645@gmail.com>
2026-03-20 02:29:31 +0100

    Add: added new method allowing to get pending emails of a specific user

--- src/modules/users/ports/user_repo_interfaces.ts
+++ src/modules/users/ports/user_repo_interfaces.ts
@@ -2,6 +2,7 @@ import { User } from "../domain/user";
 
 export interface UserRepoReader {
     getUserById(id: string): Promise<User | null>;
+    getPendingEmailByUserId(id: string): Promise<string | null>;
     getUserByUsername(username: string): Promise<User | null>;
     getUserByEmail(email: string): Promise<User | null>;
     searchUsers(query: string, limit?: number, cursor?: string): Promise<{items: User[], nextCursor?: string}>;
