4270542fe6bef175a880e00d3528bf99c17efae9
vpro3611 <vprov8645@gmail.com>
2026-03-20 01:08:26 +0100

    Update: updated this use case, it now requires verification of new email

--- src/modules/users/application/change_email_use_case.ts
+++ src/modules/users/application/change_email_use_case.ts
@@ -4,13 +4,22 @@ import {EmailAlreadyExistsError} from "../errors/email_errors";
 import {UserMapper} from "../shared/map_to_dto";
 import {UserLookup} from "../shared/user_exists_by_id";
 import {EmailAlreadyExistDatabaseError, UserDatabaseError} from "../errors/user_database_error";
+import {
+    EmailSenderInterface
+} from "../../infrasctructure/ports/email_verif_infra/email_verification/email_sender_interface";
+import crypto from "node:crypto";
+import {
+    EmailVerificationInterface
+} from "../../infrasctructure/ports/email_verif_infra/email_verification/email_verification_interface";
+import {SendVerifEmailShared} from "../shared/send_verif_email_shared";
 
 
 export class ChangeEmailUseCase {
     constructor(private readonly userRepoReader: UserRepoReader,
                 private readonly userRepoWriter: UserRepoWriter,
                 private readonly mapper: UserMapper,
-                private readonly userLookup: UserLookup
+                private readonly userLookup: UserLookup,
+                private readonly sendEmailVerifShared: SendVerifEmailShared,
     ) {}
 
 
@@ -30,11 +39,12 @@ export class ChangeEmailUseCase {
 
             await this.checkUserWithSameEmail(newEmailValid.getValue());
 
-            user.setEmail(newEmailValid);
+            await this.sendEmailVerifShared.sendIt(newEmailValid.getValue(), user,
+                "/public/confirm-email-change");
 
-            const saved = await this.userRepoWriter.save(user);
+            await this.userRepoWriter.setPendingEmail(actorId, newEmailValid.getValue());
 
-            return this.mapper.mapToDto(saved);
+            return this.mapper.mapToDto(user);
         } catch (error) {
             if (error instanceof EmailAlreadyExistDatabaseError) {
                 throw new EmailAlreadyExistsError(`User with this email already exists`);
