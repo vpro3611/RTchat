4b4a9c0a943d6d59674465577caed5944ff38583
vpro3611 <vprov8645@gmail.com>
2026-03-23 03:18:43 +0100

    Small remove

--- src/modules/chat/domain/participant/participant.ts
+++ src/modules/chat/domain/participant/participant.ts
@@ -67,6 +67,13 @@ export class Participant {
         );
     }
 
+    ensureIsOwner() {
+        if (this.role !== ParticipantRole.OWNER) {
+            throw new 
+            InsufficientPermissionsError("Cannot perform this actions since not enough permissions")
+        }
+    }
+
     mute(until: Date | null) {
         this.canSendMessages = false;
         this.mutedUntil = until;
