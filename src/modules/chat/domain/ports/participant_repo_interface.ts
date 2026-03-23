59bd3afcf1ff3f92185521c36d72b63f9e0c33a5
vpro3611 <vprov8645@gmail.com>
2026-03-23 03:00:57 +0100

    Update: updated get participants interface method

--- src/modules/chat/domain/ports/participant_repo_interface.ts
+++ src/modules/chat/domain/ports/participant_repo_interface.ts
@@ -1,5 +1,6 @@
 import {Participant} from "../participant/participant";
 import {FullParticipantDto} from "../../DTO/full_participant_dto";
+import {ParticipantListDTO} from "../../DTO/participant_list_dto";
 
 
 export interface ParticipantRepoInterface {
@@ -7,6 +8,6 @@ export interface ParticipantRepoInterface {
     remove(conversationId: string, userId: string): Promise<void>;
     findParticipant(conversationId: string, userId: string): Promise<Participant | null>;
     exists(conversationId: string, userId: string): Promise<boolean>;
-    getParticipants(conversationId: string, limit?: number, cursor?: string): Promise<{items: Participant[], nextCursor?: string}>;
+    getParticipants(conversationId: string, limit?: number, cursor?: string): Promise<{items: ParticipantListDTO[], nextCursor?: string}>;
     getSpecificParticipant(conversationId: string, userId: string): Promise<FullParticipantDto | null>;
 }
