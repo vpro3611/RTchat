3a6c9e3c35c4c177636d866c3480db5ef39ed26a
vpro3611 <vprov8645@gmail.com>
2026-03-24 20:51:02 +0100

    update: updated interface, added new method

--- src/modules/users/ports/user_to_user_blocks_interface.ts
+++ src/modules/users/ports/user_to_user_blocks_interface.ts
@@ -5,5 +5,6 @@ export interface UserToUserBlocksInterface {
     unblockSpecificUser(actorId: string, targetId: string): Promise<User>;
     getFullBlacklist(actorId: string): Promise<User[]>;
     ensureBlockedExists(actorId: string, targetId: string): Promise<boolean>;
+    ensureAnyBlocksExists(actorId: string, targetId: string): Promise<boolean>
 }
 
