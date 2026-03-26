18d78aa2c5309c5dbefc77fbe88425a13a035e53
vpro3611 <vprov8645@gmail.com>
2026-03-27 00:48:12 +0100

    Add: added new methods for fetching an API and performing actions

--- frontend-chat/src/api/apis/participant_api.ts
+++ frontend-chat/src/api/apis/participant_api.ts
@@ -3,6 +3,8 @@ import {BaseUrl} from "src/base_url/base_url";
 import {AuthStore} from "stores/auth_store";
 import type {ParticipantsResponse, ParticipantResponse, MuteDuration, ParticipantRole} from "src/api/types/participant_response";
 import type {ConversationBansFrontend} from "src/api/types/conversation_bans_response";
+import type {ConversationRequestsStatusFrontend} from "src/api/types/request_status";
+import type {ConversationRequestsResponse} from "src/api/types/conversation_request_response";
 
 export const ParticipantApi = {
   // Получить список участников
@@ -41,7 +43,7 @@ export const ParticipantApi = {
 
   // Присоединиться к беседе
   joinConversation(conversationId: string) {
-    return fetchJson<ParticipantResponse>(
+    return fetchJson<ConversationRequestsResponse>(
       `${BaseUrl.apiBaseUrl}/private/conversation/${conversationId}/join`,
       {
         method: "POST",
@@ -154,6 +156,128 @@ export const ParticipantApi = {
       `${BaseUrl.apiBaseUrl}/private/conversation/${conversationId}/ban_list`,
       {
         method: "GET",
+        headers: {
+          Authorization: `Bearer ${AuthStore.accessToken}`
+        },
+      }
+    )
+  },
+
+  changeConversationRequestStatus(
+    conversationId: string,
+    requestId: string,
+    status: ConversationRequestsStatusFrontend,
+    reviewMessage: string
+  ) {
+    // /conversation/:conversationId/requests/:requestId/status
+    return fetchJson<ConversationRequestsResponse>(
+      `${BaseUrl.apiBaseUrl}/private/conversation/${conversationId}/requests/${requestId}/status`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${AuthStore.accessToken}`
+        },
+        body: JSON.stringify({status, reviewMessage}),
+      }
+    )
+  },
+
+  createConversationRequest(conversationId: string, requestMessage: string) {
+    // /conversation/:conversationId/requests/create
+    return fetchJson<ConversationRequestsResponse>(`${BaseUrl.apiBaseUrl}/private/conversation/${conversationId}/requests/create`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${AuthStore.accessToken}`
+      },
+      body: JSON.stringify({requestMessage}),
+    })
+  },
+
+  getAllRequestsListConv(conversationId: string, status?: ConversationRequestsStatusFrontend) {
+    const query = new URLSearchParams();
+    if (status) {
+      query.append('status', status);
+    }
+    const qs = query.toString() ? `?${query.toString()}` : '';
+    // /conversation/:conversationId/requests/get_all
+    return fetchJson<ConversationRequestsResponse[]>(
+      `${BaseUrl.apiBaseUrl}/private/conversation/${conversationId}/requests/get_all${qs}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${AuthStore.accessToken}`
+        },
+      }
+    )
+  },
+
+  getSpecificRequestGroup(conversationId: string, requestId: string) {
+    // /conversation/:conversationId/requests/:requestId/view
+    return fetchJson<ConversationRequestsResponse>(
+      `${BaseUrl.apiBaseUrl}/private/conversation/${conversationId}/requests/${requestId}/view`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${AuthStore.accessToken}`
+        },
+      }
+    )
+  },
+
+  getSpecificRequestUser(requestId: string) {
+    // /private_requests/:requestId/view
+    return fetchJson<ConversationRequestsResponse>(
+      `${BaseUrl.apiBaseUrl}/private/private_requests/${requestId}/view`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${AuthStore.accessToken}`
+        },
+      }
+    )
+  },
+
+  getRequestsUserList(status?: ConversationRequestsStatusFrontend) {
+    const query = new URLSearchParams();
+    if (status) {
+      query.append('status', status);
+    }
+    const qs = query.toString() ? `?${query.toString()}` : '';
+    // private_requests/view_all
+    return fetchJson<ConversationRequestsResponse[]>(
+      `${BaseUrl.apiBaseUrl}/private/private_requests/view_all${qs}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${AuthStore.accessToken}`
+        },
+      }
+    )
+  },
+
+  removeRequestUser(requestId: string) {
+    // /private_requests/:requestId/remove PATCH
+
+    return fetchJson<void>(
+      `${BaseUrl.apiBaseUrl}/private/private_requests/${requestId}/remove`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${AuthStore.accessToken}`
+        },
+      }
+    )
+  },
+
+  withdrawRequestUser(requestId: string) {
+    // /private_requests/:requestId/withdraw PATCH
+
+    return fetchJson<ConversationRequestsResponse>(
+      `${BaseUrl.apiBaseUrl}/private/private_requests/${requestId}/withdraw`,
+      {
+        method: "PATCH",
         headers: {
           Authorization: `Bearer ${AuthStore.accessToken}`
         },
