2403e0f44ec1587bbc7f5b83046ed3146b3c9869
vpro3611 <vprov8645@gmail.com>
2026-03-27 02:59:38 +0100

    Add: added new feature - force add a participant to a group. Sort of admin add

---
+++ src/modules/chat/controllers/participant/add_participant_to_a_conversation_controller.ts
@@ -0,0 +1,33 @@
+import {Request, Response} from "express";
+import {z} from "zod";
+import {
+    AddParticipantToConversationTxService
+} from "../../transactional_services/participant/add_participant_to_conversation_tx_service";
+import {ExtractActorId} from "../../shared/extract_actor_id_req";
+
+export const AddParticipantToAConversationParamsSchema = z.object({
+    conversationId: z.string().uuid(),
+    targetId: z.string().uuid(),
+})
+
+type AddParticipantToAConversationSchemaType = z.infer<typeof AddParticipantToAConversationParamsSchema>;
+
+export class AddParticipantToAConversationController {
+    constructor(private readonly addParticipantService: AddParticipantToConversationTxService,
+                private readonly extractActorId: ExtractActorId
+    ) {}
+    addParticipantToAConversationCont =
+        async (req: Request<AddParticipantToAConversationSchemaType>, res: Response) => {
+            const actorId = this.extractActorId.extractActorId(req);
+
+            const {conversationId, targetId} = req.params;
+
+            const result = await this.addParticipantService.addParticipantToConversationTxService(
+                actorId.sub,
+                targetId,
+                conversationId
+            );
+
+            return res.status(201).json(result);
+    }
+}
