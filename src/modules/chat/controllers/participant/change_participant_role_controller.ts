ec796991483be2d49b314913052c9e6e21ae10bd
vpro3611 <vprov8645@gmail.com>
2026-03-28 22:23:27 +0100

    feat(backend): emit updated participant data via websocket for sync

--- src/modules/chat/controllers/participant/change_participant_role_controller.ts
+++ src/modules/chat/controllers/participant/change_participant_role_controller.ts
@@ -2,6 +2,7 @@ import {ChangeParticipantRoleTxService} from "../../transactional_services/parti
 import {ExtractActorId} from "../../shared/extract_actor_id_req";
 import {Request, Response} from "express";
 import {z} from "zod";
+import {Server} from "socket.io";
 
 export const ChangeParticipantRoleParamsSchema = z.object({
     conversationId: z.string().uuid(),
@@ -12,7 +13,8 @@ type ChangeParticipantRoleSchemaType = z.infer<typeof ChangeParticipantRoleParam
 
 export class ChangeParticipantRoleController {
     constructor(private readonly changeParticipantRoleService: ChangeParticipantRoleTxService,
-                private readonly extractActorId: ExtractActorId
+                private readonly extractActorId: ExtractActorId,
+                private readonly io: Server
     ) {}
 
 
@@ -26,6 +28,11 @@ export class ChangeParticipantRoleController {
             conversationId,
             targetId
         )
+
+        this.io.to(conversationId).emit("participant:updated", {
+            conversationId,
+            participant: result
+        });
 
         return res.status(200).json(result);
     }
