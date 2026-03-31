40ccde7dc4840e51389782980d5358f677f151fa
vpro3611 <vprov8645@gmail.com>
2026-03-31 16:29:56 +0200

    backend: update transactional services to include conversation repository

--- src/modules/chat/transactional_services/message/get_specific_message_service.ts
+++ src/modules/chat/transactional_services/message/get_specific_message_service.ts
@@ -7,31 +7,29 @@ import {FindMessageById} from "../../shared/find_message_by_id";
 import {ParticipantRepositoryPg} from "../../repositories_pg_realization/participant_repository_pg";
 import {GetSpecificMessageUseCase} from "../../application/message/get_specific_message_use_case";
 import {RedisCacheService} from "../../../../container";
+import {ConversationRepositoryPg} from "../../repositories_pg_realization/conversation_repository_pg";
 
 
 export class GetSpecificMessageService {
     constructor(private readonly txManager: TransactionManagerInterface) {}
 
-
     async getSpecificMessageService(actorId: string, conversationId: string, messageId: string) {
         return await this.txManager.runInTransaction(async (client) => {
             const messageRepo = new MessageRepositoryPg(client);
+            const conversationRepo = new ConversationRepositoryPg(client);
             const messageMapper = new MapToMessage();
-            const findMessageById = new FindMessageById(messageRepo);
             const participantRepo = new ParticipantRepositoryPg(client);
+            const findMessageById = new FindMessageById(messageRepo);
 
             const getSpecificMessageUseCase = new GetSpecificMessageUseCase(
                 messageMapper,
                 findMessageById,
                 participantRepo,
-                RedisCacheService
+                RedisCacheService,
+                conversationRepo
             );
 
-            return await getSpecificMessageUseCase.getSpecificMessageUseCase(
-                actorId,
-                conversationId,
-                messageId
-            );
+            return await getSpecificMessageUseCase.getSpecificMessageUseCase(actorId, conversationId, messageId);
         })
     }
 }
