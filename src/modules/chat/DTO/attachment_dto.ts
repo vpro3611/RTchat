940ab34820b04d1a15123d07b9b7896bc5b9ddf2
vpro3611 <vprov8645@gmail.com>
2026-04-02 22:30:17 +0200

    feat: add DTOs for message attachments

---
+++ src/modules/chat/DTO/attachment_dto.ts
@@ -0,0 +1,11 @@
+import {AttachmentType} from "../domain/message/attachment";
+
+export type AttachmentDTO = {
+    id: string;
+    blobId: string;
+    type: AttachmentType;
+    name: string;
+    mimeType: string;
+    size: number;
+    createdAt: string;
+}
---
+++ src/modules/chat/DTO/file_dto.ts
@@ -0,0 +1,6 @@
+export interface FileDTO {
+    buffer: Buffer;
+    originalname: string;
+    mimetype: string;
+    size: number;
+}
