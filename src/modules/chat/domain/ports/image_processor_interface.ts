966ac72d987692ef83a6350556e4483f66f99520
vpro3611 <vprov8645@gmail.com>
2026-04-02 22:30:17 +0200

    feat: update interfaces and errors for attachments

--- src/modules/chat/domain/ports/image_processor_interface.ts
+++ src/modules/chat/domain/ports/image_processor_interface.ts
@@ -1,3 +1,4 @@
 export interface ImageProcessorInterface {
     processAvatar(buffer: Buffer): Promise<{ data: Buffer; mimeType: string }>;
+    processImage(buffer: Buffer): Promise<{ data: Buffer; mimeType: string }>;
 }
