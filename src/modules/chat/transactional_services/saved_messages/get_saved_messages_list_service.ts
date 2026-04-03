6622c1c29943284982f898114e9b86f74261519a
vpro3611 <vprov8645@gmail.com>
2026-04-03 19:22:33 +0200

    refactor: update saved messages transactional services

--- src/modules/chat/transactional_services/saved_messages/get_saved_messages_list_service.ts
+++ src/modules/chat/transactional_services/saved_messages/get_saved_messages_list_service.ts
@@ -6,16 +6,20 @@ import {SavedMessagesRepoPg} from "../../repositories_pg_realization/saved_messa
 import {MapToSavedMessageDto} from "../../shared/map_to_saved_message_dto";
 import {GetSavedMessagesListUseCase} from "../../application/saved_messages/get_saved_messages_list_use_case";
 import {RedisCacheService} from "../../../../container";
+import {EncryptionPort} from "../../../infrasctructure/ports/encryption/encryption_port";
 
 
 export class GetSavedMessagesListService {
-    constructor(private readonly txManager: TransactionManagerInterface) {}
+    constructor(
+        private readonly txManager: TransactionManagerInterface,
+        private readonly encryptionService: EncryptionPort
+    ) {}
 
 
     async getSavedMessagesListService(actorId: string, limit?: number, cursor?: string) {
         return await this.txManager.runInTransaction(async (client) => {
             const userRepoReader = new UserRepoReaderPg(client);
-            const savedMessageRepo = new SavedMessagesRepoPg(client);
+            const savedMessageRepo = new SavedMessagesRepoPg(client, this.encryptionService);
             const mapToSavedMessageDto = new MapToSavedMessageDto();
 
             const getSavedMessagesListUseCase = new GetSavedMessagesListUseCase(
--- src/modules/chat/transactional_services/saved_messages/remove_saved_message_service.ts
+++ src/modules/chat/transactional_services/saved_messages/remove_saved_message_service.ts
@@ -5,16 +5,20 @@ import {UserRepoReaderPg} from "../../../users/repositories/user_repo_reader_pg"
 import {SavedMessagesRepoPg} from "../../repositories_pg_realization/saved_messages_repo_pg";
 import {RemoveSavedMessageUseCase} from "../../application/saved_messages/remove_saved_message_use_case";
 import {RedisCacheService} from "../../../../container";
+import {EncryptionPort} from "../../../infrasctructure/ports/encryption/encryption_port";
 
 
 export class RemoveSavedMessageService {
-    constructor(private readonly txManager: TransactionManagerInterface) {}
+    constructor(
+        private readonly txManager: TransactionManagerInterface,
+        private readonly encryptionService: EncryptionPort
+    ) {}
 
 
     async removeSavedMessageService(actorId: string, messageId: string) {
         return await this.txManager.runInTransaction(async (client) => {
             const userRepoReader = new UserRepoReaderPg(client);
-            const savedMessageRepo = new SavedMessagesRepoPg(client);
+            const savedMessageRepo = new SavedMessagesRepoPg(client, this.encryptionService);
 
             const removeSavedMessageUseCase = new RemoveSavedMessageUseCase(
                 userRepoReader,
