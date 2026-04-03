43f4fea3c8e0d32cf32eaa7b1dc558f5f6c49429
vpro3611 <vprov8645@gmail.com>
2026-04-03 19:23:00 +0200

    refactor: update conversation requests services (batch 3)

--- src/modules/chat/transactional_services/conversation_requests/remove_request_service.ts
+++ src/modules/chat/transactional_services/conversation_requests/remove_request_service.ts
@@ -5,15 +5,16 @@ import {UserRepoReaderPg} from "../../../users/repositories/user_repo_reader_pg"
 import {ConversationRequestsRepositoryPg} from "../../repositories_pg_realization/conversation_requests_repository_pg";
 import {RemoveRequestUseCase} from "../../application/conversation_requests/remove_request_use_case";
 import {RedisCacheService} from "../../../../container";
+import {EncryptionPort} from "../../../infrasctructure/ports/encryption/encryption_port";
 
 
 export class RemoveRequestService {
-    constructor(private readonly txManager: TransactionManagerInterface) {}
+    constructor(private readonly txManager: TransactionManagerInterface, private readonly encryptionService: EncryptionPort) {}
 
     async removeRequestService(actorId: string, requestId: string) {
         return await this.txManager.runInTransaction(async (client) => {
             const userRepoReader = new UserRepoReaderPg(client);
-            const conversationRequestsRepo = new ConversationRequestsRepositoryPg(client);
+            const conversationRequestsRepo = new ConversationRequestsRepositoryPg(client, this.encryptionService);
 
             const removeRequestUseCase = new RemoveRequestUseCase(
                 userRepoReader,
