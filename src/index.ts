73f40c5e8fbb82a582f52fb2669e85ce05894ca6
vpro3611 <vprov8645@gmail.com>
2026-07-03 19:03:35 +0200

    add: added an option for node to resolve to ipv4 first

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import {startServer} from "./server";
-import {connectRedis} from "./modules/infrasctructure/ports/cache_service/connect_reddis";
+import dns from "node:dns";
 
+dns.setDefaultResultOrder("ipv4first");
+
+import { startServer } from "./server";
+import { connectRedis } from "./modules/infrasctructure/ports/cache_service/connect_reddis";
 
 async function main() {
-    await connectRedis();
-    await startServer();
+  await connectRedis();
+  await startServer();
 }
 
 main().catch((err) => {
-    console.error(err);
-    process.exit(1);
+  console.error(err);
+  process.exit(1);
 });
