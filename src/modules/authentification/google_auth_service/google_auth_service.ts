265f6c321c0f060fea8b1d8b08a3187684e749a0
vpro3611 <vprov8645@gmail.com>
2026-07-03 17:28:28 +0200

    fix: fixed { error"

--- src/modules/authentification/google_auth_service/google_auth_service.ts
+++ src/modules/authentification/google_auth_service/google_auth_service.ts
@@ -1,35 +1,36 @@
-import {GoogleAuthServiceInterface} from "../ports/google_auth_service_interface";
-import {OAuth2Client} from "google-auth-library";
-import {InvalidTokenJWTError} from "../errors/token_errors";
+import { GoogleAuthServiceInterface } from "../ports/google_auth_service_interface";
+import { OAuth2Client } from "google-auth-library";
+import { InvalidTokenJWTError } from "../errors/token_errors";
 
 export class GoogleAuthService implements GoogleAuthServiceInterface {
-    private client: OAuth2Client;
+  private client: OAuth2Client;
 
-    constructor(private readonly clientId: string) {
-        this.client = new OAuth2Client(clientId);
+  constructor(private readonly clientId: string) {
+    this.client = new OAuth2Client(clientId);
+  }
+
+  async verifyIdToken(idToken: string): Promise<string> {
+    try {
+      const ticket = await this.client.verifyIdToken({
+        idToken: idToken,
+        audience: this.clientId,
+      });
+      const payload = ticket.getPayload();
+      if (!payload || !payload.email) {
+        throw new InvalidTokenJWTError(
+          "Invalid Google ID token payload or missing email",
+        );
+      }
+      return payload.email;
+    } catch (e: any) {
+      console.error("Google verify error:");
+      console.error(e);
+
+      if (e.response?.data) {
+        console.error(e.response.data);
+      }
+
+      throw e;
     }
-
-    async verifyIdToken(idToken: string): Promise<string> {
-        try {
-            const ticket = await this.client.verifyIdToken({
-                idToken: idToken,
-                audience: this.clientId,
-            });
-            const payload = ticket.getPayload();
-            if (!payload || !payload.email) {
-                throw new InvalidTokenJWTError("Invalid Google ID token payload or missing email");
-            }
-            return payload.email;
-        } catch (e: any) {
-            console.error("Google verify error:");
-            console.error(e);
-
-            if (e.response?.data) {
-                console.error(e.response.data);
-            }
-
-            throw e;
-        }
-        }
-    }
+  }
 }
